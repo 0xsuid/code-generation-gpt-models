a22e5d6f820c65240e3231166114c79c765b998e
Godhani Harsh <45615666+0xsuid@users.noreply.github.com>
2023-02-16 15:22:52 +0100

    Update generate.sh - remove comments

--- generate.sh
+++ generate.sh
@@ -26,28 +26,28 @@ python3 generate_code.py \
         -t "0xsuid/simba-1.3b" \
         -m "0xsuid/simba-1.3b"
 
-# Generate codes for GPT Neo 125M model
+Generate codes for GPT Neo 125M model
 
-# python3 generate_code.py \
-#         --limit 100 \
-#         --random \
-#         --difficulties "introductory" \
-#         --save "generated_codes_125M_100_random_problems_introductory.json" \
-#         -t "EleutherAI/gpt-neo-125M" \
-#         -m "0xsuid/simba-125M"
+python3 generate_code.py \
+        --limit 100 \
+        --random \
+        --difficulties "introductory" \
+        --save "generated_codes_125M_100_random_problems_introductory.json" \
+        -t "EleutherAI/gpt-neo-125M" \
+        -m "0xsuid/simba-125M"
 
-# python3 generate_code.py \
-#         --limit 100 \
-#         --random \
-#         --difficulties "interview" \
-#         --save "generated_codes_125M_100_random_problems_interview.json" \
-#         -t "EleutherAI/gpt-neo-125M" \
-#         -m "0xsuid/simba-125M"
+python3 generate_code.py \
+        --limit 100 \
+        --random \
+        --difficulties "interview" \
+        --save "generated_codes_125M_100_random_problems_interview.json" \
+        -t "EleutherAI/gpt-neo-125M" \
+        -m "0xsuid/simba-125M"
 
-# python3 generate_code.py \
-#         --limit 100 \
-#         --random \
-#         --difficulties "competition" \
-#         --save "generated_codes_125M_100_random_problems_competition.json" \
-#         -t "EleutherAI/gpt-neo-125M" \
-#         -m "0xsuid/simba-125M"
+python3 generate_code.py \
+        --limit 100 \
+        --random \
+        --difficulties "competition" \
+        --save "generated_codes_125M_100_random_problems_competition.json" \
+        -t "EleutherAI/gpt-neo-125M" \
+        -m "0xsuid/simba-125M"
