8b5c3f73dc871c30c510ede57b725b6af21d62e1
0xsuid <0xsuid@protonmail.com>
2023-02-18 18:44:12 +0000

    Add Generated code

--- eval/eval.py
+++ eval/eval.py
@@ -2,7 +2,7 @@ import json
 from evaluate import load
 
 apps_metric = load('codeparrot/apps_metric')
-generated_code = json.load(open("/home/user/code-generation-gpt-models/eval/generated_codes_1_3B_100_problems_introductory.json", "r"))
+generated_code = json.load(open("/home/user/code-generation-gpt-models/eval/generated_codes_1_3B_all_problems_introductory.json", "r"))
 solutions = []
 
 for problem in generated_code.values():
--- eval/generate.sh
+++ eval/generate.sh
@@ -2,46 +2,41 @@
 
 # Generate codes for GPT Neo 1.3B model
 
-python3 generate_code.py \
-        --limit 100 \
-        --difficulties "introductory" \
-        --save "generated_codes_1_3B_100_problems_introductory.json" \
-        -t "0xsuid/simba-1.3b" \
-        -m "0xsuid/simba-1.3b"
+# python3 generate_code.py \
+#         --difficulties "introductory" \
+#         --save "generated_codes_1_3B_all_problems_introductory.json" \
+#         -t "0xsuid/simba-1.3b" \
+#         -m "0xsuid/simba-1.3b"
+
+# python3 generate_code.py \
+#         --difficulties "interview" \
+#         --save "generated_codes_1_3B_100_problems_interview.json" \
+#         -t "0xsuid/simba-1.3b" \
+#         -m "0xsuid/simba-1.3b"
 
 python3 generate_code.py \
-        --limit 100 \
-        --difficulties "interview" \
-        --save "generated_codes_1_3B_100_problems_interview.json" \
-        -t "0xsuid/simba-1.3b" \
-        -m "0xsuid/simba-1.3b"
-
-python3 generate_code.py \
-        --limit 100 \
         --difficulties "competition" \
-        --save "generated_codes_1_3B_100_problems_competition.json" \
+        --save "generated_codes_1_3B_all_problems_competition.json" \
         -t "0xsuid/simba-1.3b" \
         -m "0xsuid/simba-1.3b"
 
 # Generate codes for GPT Neo 125M model
 
 python3 generate_code.py \
-        --limit 100 \
         --difficulties "introductory" \
-        --save "generated_codes_125M_100_problems_introductory.json" \
+        --save "generated_codes_125M_all_problems_introductory.json" \
         -t "EleutherAI/gpt-neo-125M" \
         -m "0xsuid/simba-125M"
 
 python3 generate_code.py \
-        --limit 100 \
         --difficulties "interview" \
-        --save "generated_codes_125M_100_problems_interview.json" \
+        --save "generated_codes_125M_all_problems_interview.json" \
         -t "EleutherAI/gpt-neo-125M" \
         -m "0xsuid/simba-125M"
 
-python3 generate_code.py \
-        --limit 100 \
-        --difficulties "competition" \
-        --save "generated_codes_125M_100_problems_competition.json" \
-        -t "EleutherAI/gpt-neo-125M" \
-        -m "0xsuid/simba-125M"
+# python3 generate_code.py \
+#         --limit 100 \
+#         --difficulties "competition" \
+#         --save "generated_codes_125M_100_problems_competition.json" \
+#         -t "EleutherAI/gpt-neo-125M" \
+#         -m "0xsuid/simba-125M"
