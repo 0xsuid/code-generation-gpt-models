d5cc9546f5de38d6e4759b313cbd7a0511be2f6b
0xsuid <0xsuid@protonmail.com>
2023-02-25 02:39:34 +0000

    eval.py - add file and difficulty args

--- eval/eval.py
+++ eval/eval.py
@@ -1,14 +1,22 @@
 import json
 from evaluate import load
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+parser.add_argument("-f", "--file", dest="file", default='generated_codes.json',
+                    help="Generated code file path")
+parser.add_argument("-d", "--difficulty", dest="difficulty", choices=["all","introductory","interview","competition"],
+                    default="all", help="difficulties - introductory, interview & competition")
+args = parser.parse_args()
 
 apps_metric = load('codeparrot/apps_metric')
-generated_code = json.load(open("/home/user/code-generation-gpt-models/eval/generated_codes_1_3B_all_problems_introductory.json", "r"))
+generated_code = json.load(open(args.file, "r"))
 solutions = []
 
 for problem in generated_code.values():
     solutions.append([problem['answer']])
 
 # to evaluate generations made for all levels for example
-results = apps_metric.compute(predictions=solutions, level="introductory")
+results = apps_metric.compute(predictions=solutions, level=args.difficulty)
 
 print(results)
